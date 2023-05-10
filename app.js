b75fa754e0d9391a6292cda56dc7464254321987
Emmabata <ndinezeemmanuel@gmail.com>
2023-05-10 09:59:24 +0100

    update

---
+++ app.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose')
+const User = require('./User')
+const { ObjectId } = require('bson')
+
+mongoose.connect('mongodb+srv://Bata79:rzCTVsKuTYFSyLEy@cluster0.qh6q42p.mongodb.net/')
