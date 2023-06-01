1db01b87cfa12348596a5cfe7d526ff5bdd93acd
Emmabata <ndinezeemmanuel@gmail.com>
2023-06-01 13:02:26 +0100

    update

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose')
 const Person = require('./Models/Person')
 
 mongoose.connect(process.env.MONGO_URL)
+
+
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
